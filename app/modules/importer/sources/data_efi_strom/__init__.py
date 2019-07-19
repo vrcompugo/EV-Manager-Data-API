c8f7f2c3390f9adee92af20aa8e5b3107b9db4bc
Andreas Hedderich <a.hedderich@hbundb.de>
2019-07-19 13:55:10 +0200

    reseller import data.efi

--- app/modules/importer/sources/data_efi_strom/__init__.py
+++ app/modules/importer/sources/data_efi_strom/__init__.py
@@ -4,24 +4,24 @@ def run_import_by_model(model, remote_id=None, local_id=None):
 
     if model is None or model == "customer":
         from .customer import run_import
-        return run_import()
+        run_import()
 
     if model is None or model == "reseller":
         from .reseller import run_import
-        return run_import()
+        run_import()
 
     if model is None or model == "survey":
         from .survey import run_import
-        return run_import()
+        run_import()
 
     if model is None or model == "product":
         from .product import run_import
-        return run_import()
+        run_import()
 
     if model is None or model == "offer":
         from .offer import run_import
-        return run_import(remote_id=remote_id, local_id=local_id)
+        run_import(remote_id=remote_id, local_id=local_id)
 
     if model is None or model == "contract":
         from .contract import run_import
-        return run_import()
+        run_import()
--- app/modules/importer/sources/orgamaxx/__init__.py
+++ app/modules/importer/sources/orgamaxx/__init__.py
@@ -4,4 +4,4 @@ def run_import_by_model(model, remote_id=None, local_id=None):
 
     if model is None or model == "customer":
         from .customer import run_import
-        return run_import()
+        run_import()
