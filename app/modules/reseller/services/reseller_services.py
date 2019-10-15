eceaa1b3c7a532c64bba31811da921f2b7221fd1
Andreas Hedderich <a.hedderich@hbundb.de>
2019-10-15 15:48:05 +0200

    automated welcome email

--- app/modules/reseller/services/reseller_services.py
+++ app/modules/reseller/services/reseller_services.py
@@ -1,7 +1,8 @@
 import datetime
+from sqlalchemy import or_
 
 from app import db
-from app.models import UserRole
+from app.models import UserRole, User
 from app.modules.user.user_services import add_item as add_user_item
 from app.exceptions import ApiException
 from app.utils.get_items_by_model import get_items_by_model, get_one_item_by_model
@@ -12,12 +13,14 @@ from ..models.reseller import Reseller, ResellerSchema
 
 def add_item(data):
     sales_role = db.session.query(UserRole).filter(UserRole.code == "sales").one()
-    user = add_user_item({
-        "username": data["email"],
-        "password": "test",
-        "email": data["email"],
-        "roles": [sales_role.id]
-    })
+    user = User.query.filter(or_(User.username == data["username"], User.email == data["email"])).first()
+    if user is None:
+        user = add_user_item({
+            "username": data["email"],
+            "password": "test",
+            "email": data["email"],
+            "roles": [sales_role.id]
+        })
     new_item = Reseller()
     new_item = set_attr_by_dict(new_item, data, ["id"])
     new_item.user_id = user.id
