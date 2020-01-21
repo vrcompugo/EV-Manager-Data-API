f449b3dea7193091587b68359e630d13993410ed
Andreas Hedderich <a.hedderich@hbundb.de>
2020-01-21 19:32:17 +0100

    bugfix set attr

--- app/utils/set_attr_by_dict.py
+++ app/utils/set_attr_by_dict.py
@@ -5,6 +5,8 @@ def set_attr_by_dict(item, data, blacklist=None, fields=None, merge=False):
         fields = data.keys()
     for field in fields:
         if field in data and hasattr(item, field) and (blacklist is None or field not in blacklist):
+            if str(field) == "_sa_instance_state":
+                continue
             if merge:
                 if str(data[field]) != "" and str(data[field]) != "0":
                     setattr(item, field, data[field])
