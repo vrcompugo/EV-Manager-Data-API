c643b1b3eceb012707a5aed91f57792f52bd5243
Andreas Hedderich <a.hedderich@hbundb.de>
2020-01-23 13:00:11 +0100

    commission command update

--- commands/update_commission_values.py
+++ commands/update_commission_values.py
@@ -1,3 +1,5 @@
+from sqlalchemy import or_
+
 from app import db
 from app.models import Lead
 
@@ -7,14 +9,41 @@ from ._progress import printProgressBar
 def update_commission_values():
     uncounted_leads = db.session.query(Lead)\
         .filter(Lead.counted_at.is_(None))\
+        .filter(or_(
+            Lead.contact_source == "DAA",
+            Lead.contact_source == "DAA/PV",
+            Lead.contact_source == "WattFox ",
+            Lead.contact_source == "WattFox",
+            Lead.contact_source == "Wattfox",
+            Lead.contact_source == "Senec Anfrage",
+            Lead.contact_source == "Senec",
+        ))\
         .all()
     returned_leads = db.session.query(Lead)\
         .filter(Lead.status == "returned")\
         .filter(Lead.returned_at.is_(None))\
+        .filter(or_(
+            Lead.contact_source == "DAA",
+            Lead.contact_source == "DAA/PV",
+            Lead.contact_source == "WattFox ",
+            Lead.contact_source == "WattFox",
+            Lead.contact_source == "Wattfox",
+            Lead.contact_source == "Senec Anfrage",
+            Lead.contact_source == "Senec",
+        ))\
         .all()
     won_leads = db.session.query(Lead)\
         .filter(Lead.status == "won")\
         .filter(Lead.won_at.is_(None))\
+        .filter(or_(
+            Lead.contact_source == "DAA",
+            Lead.contact_source == "DAA/PV",
+            Lead.contact_source == "WattFox ",
+            Lead.contact_source == "WattFox",
+            Lead.contact_source == "Wattfox",
+            Lead.contact_source == "Senec Anfrage",
+            Lead.contact_source == "Senec",
+        ))\
         .all()
     total = len(uncounted_leads) + len(returned_leads) + len(won_leads)
     printProgressBar(0, total, prefix='Progress:', suffix='Complete', length=50)
