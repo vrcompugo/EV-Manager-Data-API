388fa15926dfde6d3c5d48e4e933512fc3ce8e7e
Andreas Hedderich <a.hedderich@hbundb.de>
2020-01-23 11:57:03 +0100

    error handling

--- app/utils/error_handler.py
+++ app/utils/error_handler.py
@@ -6,11 +6,13 @@ from app.config import email_config
 
 def error_handler():
     trace_output = traceback.format_exc()
-    print(traceback)
-    message = Message(text=message,
-                      subject="Error EV-Manager-API:" + message,
+    print(trace_output)
+    message = Message(text=trace_output,
+                      subject="Error EV-Manager-API",
                       mail_from=("EV-Manager", "bugs@api.korbacher-energiezentrum.de"),
                       config=email_config)
 
     message.mail_to = "a.hedderich@hbb-werbung.de"
     message.send()
+    trace_output = traceback.format_exc()
+    print(trace_output)
