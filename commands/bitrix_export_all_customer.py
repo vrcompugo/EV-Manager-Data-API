494c2dad2c14f1869f3b167b7854967f7ca29dba
Andreas Hedderich <a.hedderich@hbundb.de>
2020-03-10 13:52:25 +0100

    customer_number bititx export

--- commands/bitrix_export_all_customer.py
+++ commands/bitrix_export_all_customer.py
@@ -2,21 +2,23 @@ from sqlalchemy import and_
 
 from app import db
 from app.models import Customer, ImportIdAssociation
-from app.modules.importer.sources.bitrix24.customer import run_export, find_association
+from app.modules.importer.sources.bitrix24.customer import run_export, find_association, post
 
 from ._progress import printProgressBar
 
 
 def bitrix_export_all_customer():
-    customers = db.session.query(Customer)\
-        .outerjoin(ImportIdAssociation, Customer.id == ImportIdAssociation.local_id)\
-        .all()
+    customers = db.session.query(Customer).all()
     total = len(customers)
     printProgressBar(0, total, prefix='Progress:', suffix='Complete', length=50)
     i = 0
     for customer in customers:
         link = find_association("Customer", local_id=customer.id)
-        if link is None:
-            run_export(local_id=customer.id)
+        if link is not None:
+            if customer.customer_number is not None and customer.customer_number != "":
+                response = post("crm.contact.update", post_data={
+                    "id": link.remote_id,
+                    "fields[UF_CRM_1572949928]": customer.customer_number
+                })
         i = i + 1
         printProgressBar(i, total, prefix='Progress:', suffix='Complete', length=50)
