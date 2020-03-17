15af9d2cf3e3688fd5e46d42249f84836c21e513
Andreas Hedderich <a.hedderich@hbundb.de>
2020-03-17 09:17:35 +0100

    reseller geocode addr

--- app/modules/reseller/services/reseller_services.py
+++ app/modules/reseller/services/reseller_services.py
@@ -7,6 +7,7 @@ from app.modules.user.user_services import add_item as add_user_item
 from app.exceptions import ApiException
 from app.utils.get_items_by_model import get_items_by_model, get_one_item_by_model
 from app.utils.set_attr_by_dict import set_attr_by_dict
+from app.utils.google_geocoding import geocode_address
 
 from ..models.reseller import Reseller, ResellerSchema
 
