34f6c529bb15a745bd8fc54dd8d460728a866dbd
Andreas Hedderich <a.hedderich@hbundb.de>
2020-04-06 16:34:50 +0200

    fix missing customers

---
+++ commands/customer_export_missing.py
@@ -0,0 +1,16 @@
+import datetime
+
+from app import db
+from app.models import Customer
+from app.modules.importer.sources.bitrix24._association import find_association, associate_item
+from app.modules.importer.sources.bitrix24.customer import run_export
+
+
+def customer_export_missing():
+    customers = db.session.query(Customer).all()
+
+    for customer in customers:
+        link = find_association("Customer", local_id=customer.id)
+        if link is None:
+            print(customer.id)
+            run_export(local_id=customer.id)
