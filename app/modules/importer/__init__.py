2f125453cf4922731b7efca687cb63e23e8f5152
Andreas Hedderich <a.hedderich@hbundb.de>
2020-07-06 08:43:41 +0200

    auto import

--- app/modules/importer/__init__.py
+++ app/modules/importer/__init__.py
@@ -34,6 +34,12 @@ def cron():
     except Exception as e:
         error_handler()
 
+    from .sources.aroundhome.lead import run_cron_import
+    try:
+        run_cron_import()
+    except Exception as e:
+        error_handler()
+
     from .sources.bitrix24.lead import run_cron_export
     try:
         run_cron_export()
