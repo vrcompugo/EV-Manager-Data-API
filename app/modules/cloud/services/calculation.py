64a11aba3368a083d0f39244341cd2064d9874cb
Andreas Hedderich <a.hedderich@hbundb.de>
2020-08-19 15:07:22 +0200

    re

--- app/modules/cloud/services/calculation.py
+++ app/modules/cloud/services/calculation.py
@@ -113,7 +113,7 @@ def calculate_cloud(data):
             settings["data"]["cloud_settings"]["cloud_user_prices"][str(user["id"])]
         ))[0]["value"]
         if "name" in user and user["name"].lower() == "bsh":
-            result["cloud_price_light"] = data["power_usage"] * 0.36 / 10 / 12
+            result["cloud_price_light"] = data["power_usage"] * 0.37 / 10 / 12
         if "price_guarantee" in data and data["price_guarantee"] == "2_years":
             if 0 < data["power_usage"] <= 5000:
                 result["cloud_price_light"] = 29
