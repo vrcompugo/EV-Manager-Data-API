b4ec6f59fa7b5d7221ee93d9584ea3901db76d4a
Andreas Hedderich <a.hedderich@hbundb.de>
2020-10-01 15:26:23 +0200

    KESO-53

--- app/modules/bitrix24/bitrix24_routes.py
+++ app/modules/bitrix24/bitrix24_routes.py
@@ -5,6 +5,7 @@ from.sections.calendar import register_routes as calendar_routes
 from.sections.cloud_data import register_routes as cloud_data_routes
 from.sections.commissions import register_routes as commissions_routes
 from.sections.downloads import register_routes as download_routes
+from.sections.etermin import register_routes as etermin_routes
 from.sections.dresdner_bank import register_routes as dresdner_routes
 from.sections.resellers import register_routes as resellers_routes
 from.sections.salesportal import register_routes as salesportal_routes
@@ -18,6 +19,7 @@ cloud_data_routes(bitrix24_bp)
 commissions_routes(bitrix24_bp)
 download_routes(bitrix24_bp)
 dresdner_routes(bitrix24_bp)
+etermin_routes(bitrix24_bp)
 resellers_routes(bitrix24_bp)
 salesportal_routes(bitrix24_bp)
 settings_routes(bitrix24_bp)
---
+++ app/modules/bitrix24/sections/etermin.py
@@ -0,0 +1,40 @@
+import jwt
+import datetime
+from flask import Blueprint, render_template, request, make_response
+
+from app.config import key
+
+from ..utils import get_bitrix_auth_info
+
+
+def register_routes(api: Blueprint):
+
+    @api.route("/etermin", methods=["GET", "POST"])
+    def etermin_iframe():
+        auth_info = get_bitrix_auth_info(request)
+        if "user2" not in auth_info:
+            return "Forbidden"
+        encoded_jwt = jwt.encode(
+            payload={
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=300),
+                'iat': datetime.datetime.utcnow(),
+                'sub': auth_info["user2"].id
+            },
+            key=key,
+            algorithm='HS256')
+        return render_template("etermin/iframe.html", encoded_jwt=encoded_jwt.decode())
+
+    @api.route("/install", methods=["GET", "POST"])
+    def etermin_install():
+        auth_info = get_bitrix_auth_info(request)
+        if "user2" not in auth_info:
+            return "Forbidden"
+        encoded_jwt = jwt.encode(
+            payload={
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=300),
+                'iat': datetime.datetime.utcnow(),
+                'sub': auth_info["user2"].id
+            },
+            key=key,
+            algorithm='HS256')
+        return render_template("etermin/install.html", encoded_jwt=encoded_jwt.decode())
