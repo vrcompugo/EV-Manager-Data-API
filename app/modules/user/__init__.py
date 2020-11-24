416eeaa98eeb201c45535f8b7407e0897449ee8b
Andreas Hedderich <a.hedderich@hbundb.de>
2020-11-24 15:18:56 +0100

    re

--- app/modules/user/__init__.py
+++ app/modules/user/__init__.py
@@ -3,20 +3,20 @@ import datetime
 from sqlalchemy import func, or_, and_
 
 from app import db
-from app.modules.external.bitrix24.deal import get_deal, update_deal
+from app.modules.external.bitrix24.lead import get_lead, update_lead
 from app.modules.external.bitrix24.contact import get_contact, update_contact
 
 from .models.user_zip_association import UserZipAssociation
 
 
-def auto_assign_lead_to_user(deal_id):
-    deal_data = get_deal(deal_id)
-    if "assigned_by_id" not in deal_data or deal_data["assigned_by_id"] != "17":
+def auto_assign_lead_to_user(lead_id):
+    lead_data = get_lead(lead_id)
+    if "assigned_by_id" not in lead_data or lead_data["assigned_by_id"] != "17":
         return None
-    if "contact_id" not in deal_data or deal_data["contact_id"] is None or int(deal_data["contact_id"]) == 0:
+    if "contact_id" not in lead_data or lead_data["contact_id"] is None or int(lead_data["contact_id"]) == 0:
         return None
 
-    contact_data = get_contact(deal_data["contact_id"])
+    contact_data = get_contact(lead_data["contact_id"])
     if "zip" not in contact_data or contact_data["zip"] is None or contact_data["zip"] == "":
         return None
 
@@ -50,8 +50,8 @@ def auto_assign_lead_to_user(deal_id):
         update_data = {"assigned_by_id": 1}
     else:
         update_data = {"assigned_by_id": user.user_id}
-    update_deal(deal_id, update_data)
-    update_contact(deal_data["contact_id"], update_data)
+    update_lead(lead_id, update_data)
+    update_contact(lead_data["contact_id"], update_data)
     if user is not None:
         if user.current_cycle_index != current_cycle_index:
             user.current_cycle_index = current_cycle_index
