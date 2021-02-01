368c85db28538490921ebdaa9109aa1a27c60890
Andreas Hedderich <a.hedderich@hbundb.de>
2021-02-01 15:54:05 +0100

    re

--- app/modules/importer/sources/bitrix24/drive.py
+++ app/modules/importer/sources/bitrix24/drive.py
@@ -5,7 +5,7 @@ import string
 
 
 from ._connector import get, post
-from app.modules.external.bitrix24.drive import create_folder_path, add_file, add_subfolder
+from app.modules.external.bitrix24.drive import create_folder_path, add_file as bitrix_add_file, add_subfolder
 
 
 def get_folder(id):
@@ -40,3 +40,14 @@ def get_file(id):
 def get_random_string(length):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(length))
+
+
+def add_file(data):
+    if 'foldername' not in data and 'customer_id' in data:
+        data['foldername'] = f"Kunde {data['customer_id']}"
+
+    file_folder_id = create_folder_path(374738, data['foldername'])
+    if file_folder_id is None:
+        return None
+
+    return bitrix_add_file(file_folder_id, data)
