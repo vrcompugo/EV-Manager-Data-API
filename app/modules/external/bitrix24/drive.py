1efc57e497891f4e52bbe4b43df791d8cafcc0ee
Andreas Hedderich <a.hedderich@hbundb.de>
2021-02-22 09:02:43 +0100

    re

--- app/modules/external/bitrix24/drive.py
+++ app/modules/external/bitrix24/drive.py
@@ -338,7 +338,8 @@ def run_legacy_folder_creation():
                 post_data = {}
                 for folder in config["folders"]:
                     if contact.get(folder["key"]) in [None, "", "0", 0]:
-                        time.sleep(30)
+                        print(f"Kunde {contact['id']}")
+                        time.sleep(10)
                         print(f"Kunde {contact['id']}")
                         subpath = f"Kunde {contact['id']}"
                         new_folder_id = create_folder_path(folder["folder_id"], subpath)
