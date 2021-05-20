9c2902a05d21916bd5c1aa1a791a7c78caf59e9d
Andreas Hedderich <a.hedderich@hbundb.de>
2021-05-20 15:07:48 +0200

    zählerschrank zs

--- app/modules/quote_calculator/quote_data.py
+++ app/modules/quote_calculator/quote_data.py
@@ -271,8 +271,11 @@ def calculate_products(data):
             quantity = 0
             if "new_power_closet" in data["data"]["extra_options"]:
                 quantity = 1
+            product_label = "Neuer Zählerschrank"
+            if kwp > 24.9:
+                product_label = "Neuer Zählerschrank (ZS)"
             add_direct_product(
-                label="Neuer Zählerschrank",
+                label=product_label,
                 category="Extra Pakete",
                 quantity=quantity,
                 products=data["products"]
