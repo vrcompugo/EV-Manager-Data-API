7ee3d286124095e6530908996fb13ecf8023ad2a
Andreas Hedderich <a.hedderich@hbundb.de>
2021-07-23 11:06:51 +0200

    re

---
+++ app/modules/external/mfr/models/mfr_log_event.py
@@ -0,0 +1,10 @@
+from app import db
+
+
+class MfrLogEvent(db.Model):
+    __tablename__ = "mfr_log_event"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    service_request_id = db.Column(db.String(200))
+    last_change = db.Column(db.DateTime)
+    data = db.Column(db.JSON)
