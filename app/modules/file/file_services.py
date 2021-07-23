bfc01fa63b50bc85c4bc6f93915830d01b78285b
Andreas Hedderich <a.hedderich@hbundb.de>
2021-07-23 10:42:00 +0200

    cloud refresh

--- app/modules/file/file_services.py
+++ app/modules/file/file_services.py
@@ -68,6 +68,23 @@ def update_item(id, data):
         raise ApiException("item_doesnt_exist", "Item doesn't exist.", 409)
 
 
+def bitrix_export_item(item):
+    if item is not None and item.bitrix_file_id is None:
+        data = {}
+        print("update file")
+        folder_id = create_folder_path(446126, f"{item.uuid}")
+        print("update file2")
+        bitrix_file_id = add_file(folder_id, data)
+        print("update file3")
+        if bitrix_file_id is None:
+            raise ApiException("upload-failed", "file upload failed", 500)
+        item.bitrix_file_id = bitrix_file_id
+        db.session.commit()
+        return item
+    else:
+        raise ApiException("item_doesnt_exist", "Item doesn't exist.", 409)
+
+
 def sync_item(data):
     item = None
     if "uuid" in data and data["uuid"] is not None and len(data["uuid"]) > 8:
