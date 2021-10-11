6765c61d78f51024dd00c3fca1330fdadcc8e2b9
Andreas Hedderich <a.hedderich@hbundb.de>
2021-10-11 12:03:17 +0200

    re

--- app/modules/cloud/services/deal.py
+++ app/modules/cloud/services/deal.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 import json
 from app import db
 
 from app.modules.external.bitrix24.deal import get_deal, get_deals, add_deal, update_deal
+from app.modules.external.bitrix24.drive import get_folder, get_folder_id
 from app.modules.external.fakturia.deal import store_json_data, load_json_data, normalize_contract_number
 from app.modules.offer.models.offer_v2 import OfferV2
 
@@ -58,3 +60,55 @@ def cron_split_cloud_contract():
         for copy_deal_data in add_deals:
             add_deal(copy_deal_data)
         update_deal(deal.get("id"), { "stage_id": "C15:21" })
+
+
+def cron_mein_portal_initial_documents():
+    print("mein_portal_initial_documents")
+    return
+    deals = get_deals({
+        "SELECT": "full",
+        "FILTER[CATEGORY_ID]": "172",
+        "FILTER[STAGE_ID]": "C172:NEW"
+    })
+    for deal in deals:
+        print(deal["id"])
+        folder_id = get_folder_id(649168, path=f"Kunde {deal['contact_id']}/Datenblätter")
+        if folder_id is None:
+            continue
+        auto_documents = get_folder(folder_id, "01 Dokumappe.pdf")
+        if len(auto_documents) > 0:
+            print("move")
+            # update_deal(deal["id"], {"STAGE_ID": "C172:1"})
+        else:
+            print("generate")
+
+    '''
+    deals = get_deals({
+        "SELECT": "full",
+        "FILTER[CATEGORY_ID]": "32",
+        "FILTER[STAGE_ID]": "C32:WON",
+        "FILTER[>DATE_MODIFY]": "2021-07-01"
+    })
+    deals2 = get_deals({
+        "SELECT": "full",
+        "FILTER[CATEGORY_ID]": "32",
+        "FILTER[STAGE_ID]": "C32:21",
+        "FILTER[>DATE_MODIFY]": "2021-07-01"
+    })
+    if deals is None:
+        deals = deals2
+    else:
+        if deals is not None:
+            deals = deals + deals2
+    for deal in deals:
+        if deal.get("auto_document_check") not in [None, "None", ""]:
+            continue
+        print(deal["id"])
+        print(deal.get("auto_document_check"))
+        folder_id = get_folder_id(649168, path=f"Kunde {deal['contact_id']}/Datenblätter")
+        if folder_id is not None:
+            auto_documents = get_folder(folder_id, "01 Dokumappe.pdf")
+            if len(auto_documents) > 0:
+                print(auto_documents)
+                print(deal["id"])
+                update_deal(deal["id"], {"auto_document_check": datetime.now()})'''
