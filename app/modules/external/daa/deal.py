323ba4ae9dc99c607841a01a3bfc0e21ba5b76d6
Andreas Hedderich <a.hedderich@hbundb.de>
2021-10-11 11:52:54 +0200

    re

--- app/modules/external/daa/deal.py
+++ app/modules/external/daa/deal.py
@@ -88,48 +88,48 @@ def run_cron_import():
         leads = get("/api/v2/leads/bought", parameters={
             "start_sale_id": highest
         })
-	if leads is not None:
-		for lead in leads:
-			if highest < int(lead["sale_id"]):
-				highest = lead["sale_id"]
-			log = find_log("Lead", identifier=lead["sale_id"])
-			if log is not None:
-				print("already imported:", lead["sale_id"])
-				continue
+    if leads is not None:
+        for lead in leads:
+            if highest < int(lead["sale_id"]):
+                highest = lead["sale_id"]
+            log = find_log("Lead", identifier=lead["sale_id"])
+            if log is not None:
+                print("already imported:", lead["sale_id"])
+                continue
 
-			data = get_import_data(lead)
-			existing_contact = get_contact_by_email(lead["email"])
-			if existing_contact is None:
-				data["lead"]["status_id"] = "NEW"
-				contact = add_contact(data["contact"])
-				if contact is False:
-					print("add contact failed", data["contact"])
-					raise Exception("add contact failed: " + json.dumps(data["contact"]))
+            data = get_import_data(lead)
+            existing_contact = get_contact_by_email(lead["email"])
+            if existing_contact is None:
+                data["lead"]["status_id"] = "NEW"
+                contact = add_contact(data["contact"])
+                if contact is False:
+                    print("add contact failed", data["contact"])
+                    raise Exception("add contact failed: " + json.dumps(data["contact"]))
 
-				data["lead"]["contact_id"] = contact["id"]
+                data["lead"]["contact_id"] = contact["id"]
 
-				if "company" in data:
-					data["company"]["contact_id"] = contact["id"]
-					company = add_company(data["company"])
-					data["lead"]["company_id"] = company["id"]
+                if "company" in data:
+                    data["company"]["contact_id"] = contact["id"]
+                    company = add_company(data["company"])
+                    data["lead"]["company_id"] = company["id"]
 
-				lead_data = add_lead(data["lead"])
+                lead_data = add_lead(data["lead"])
 
-				data["timeline_comment"]["entity_id"] = lead_data["id"]
-				add_timeline_comment(data["timeline_comment"])
+                data["timeline_comment"]["entity_id"] = lead_data["id"]
+                add_timeline_comment(data["timeline_comment"])
 
-				auto_assign_lead_to_user(lead_data["id"])
-			else:
-				print("already known", existing_contact["id"])
+                auto_assign_lead_to_user(lead_data["id"])
+            else:
+                print("already known", existing_contact["id"])
 
-				data["lead"]["status_id"] = "14"
-				data["lead"]["contact_id"] = existing_contact["id"]
-				lead_data = add_lead(data["lead"])
+                data["lead"]["status_id"] = "14"
+                data["lead"]["contact_id"] = existing_contact["id"]
+                lead_data = add_lead(data["lead"])
 
-				data["timeline_comment"]["entity_id"] = lead_data["id"]
-				add_timeline_comment(data["timeline_comment"])
-			log_item("Lead", lead["sale_id"])
-		config = get_settings("external/daa")
-		if config is not None:
-			config["last_import_id"] = highest
-		set_settings("external/daa", config)
+                data["timeline_comment"]["entity_id"] = lead_data["id"]
+                add_timeline_comment(data["timeline_comment"])
+            log_item("Lead", lead["sale_id"])
+        config = get_settings("external/daa")
+        if config is not None:
+            config["last_import_id"] = highest
+        set_settings("external/daa", config)
