5a1833da931cc5e9d1493646ca1d1e2b2f9c3f3d
Andreas Hedderich <a.hedderich@hbundb.de>
2021-11-09 10:44:35 +0100

    re

--- app/modules/fakturia/routes.py
+++ app/modules/fakturia/routes.py
@@ -22,7 +22,6 @@ def get_deal_data(deal_id):
         return {"status": "failed", "data": {}, "message": "auth failed"}
     data = get_contract_data_by_deal(deal_id)
     if data is not None:
-        print(data)
         return {"status": "success", "data": data}
     return {"status": "failed", "data": {}, "message": "history not found"}
 
