3ba1e5efa14107a2eac7634527330c082720decf
Andreas Hedderich <a.hedderich@hbundb.de>
2021-11-24 06:50:12 +0100

    re

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,15 @@
 
 cd /usr/src/app
 
+echo "80.190.117.229 api.fakturia.de" >> /etc/hosts
+
 echo $ENVIRONMENT
 if [ $ENVIRONMENT = "dev" ]
 then
-  until $@; do
+  until runuser -u appuser -- $@; do
       echo "Debug crashed. Respawning"
       sleep 5
   done
 else
-  $@
+  runuser -u appuser -- $@
 fi
