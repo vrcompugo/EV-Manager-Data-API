abc75e97cad168c2766952400841d68d06d34c00
Andreas Hedderich <a.hedderich@hbundb.de>
2022-04-19 11:45:59 +0200

    tolerate begin date don't match anual reporting

--- app/modules/external/smartme2/powermeter_measurement.py
+++ app/modules/external/smartme2/powermeter_measurement.py
@@ -1,5 +1,7 @@
+from calendar import month
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
+from dateutil.parser import parse
 
 from app import db
 
@@ -55,5 +57,36 @@ def run_cron_import():
 def get_device_by_datetime(smartme_number, datetime):
     device = get("/DeviceBySerial", parameters={ "serial": smartme_number})
     if device is None:
-        return smart2_get_device_by_datetime(smartme_number, datetime)
-    return get(f"/MeterValues/{device['Id']}", parameters={ "date": str(datetime) })
+        data = smart2_get_device_by_datetime(smartme_number, datetime)
+    else:
+        data = get(f"/MeterValues/{device['Id']}", parameters={ "date": str(datetime) })
+    if data is not None:
+        requested_date = normalize_date(datetime)
+        responded_date = normalize_date(data.get("Date"))
+        if requested_date != responded_date:
+            if responded_date <= requested_date:
+                for i in range(10):
+                    requested_date2 = requested_date + timedelta(days=i * 10)
+                    if device is None:
+                        data2 = smart2_get_device_by_datetime(smartme_number, requested_date2)
+                    else:
+                        data2 = get(f"/MeterValues/{device['Id']}", parameters={ "date": str(requested_date2) })
+                    if data2 is not None:
+                        responded_date2 = normalize_date(data2.get("Date"))
+                        if responded_date2 > requested_date:
+                            print(requested_date2, responded_date2)
+                            kwh_diff = data2.get("CounterReading") - data.get("CounterReading")
+                            days_diff = (responded_date2 - responded_date).days
+                            days_to_add = (requested_date - responded_date).days
+                            new_reading = data.get("CounterReading") + days_to_add * (kwh_diff / days_diff)
+                            data["CounterReading"] = new_reading
+                            data["Date"] = str(requested_date)
+                            return data
+
+                print(responded_date)
+                print("---------")
+    return data
+
+
+def normalize_date(datetime):
+    return parse(parse(datetime).strftime("%Y-%m-%d"))
