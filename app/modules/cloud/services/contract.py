76deb2cdf12d32421ed2ae4752cb5648559b6938
Andreas Hedderich <a.hedderich@hbundb.de>
2022-05-12 16:36:32 +0200

    re

--- app/modules/cloud/services/contract.py
+++ app/modules/cloud/services/contract.py
@@ -678,7 +678,7 @@ def get_annual_statement_data(data, year, manuell_data):
                     statement["errors"].append(f"{statement_config[product]['label']} hat keinen Verbrauch")
                 if statement_config[product]["actual_usage"] < statement_config[product]["actual_usage_net"]:
                     statement["errors"].append(f"{statement_config[product]['label']} Netzbezug ist größer als der Gesamtverbrauch")
-                if statement_config[product]["actual_usage_net"]/statement_config[product]["actual_usage"] > 0.8 :
+                if statement_config[product]["actual_usage"] > 0 and statement_config[product]["actual_usage_net"]/statement_config[product]["actual_usage"] > 0.8 :
                     statement["errors"].append(f"{statement_config[product]['label']} Netzbezug ist mehr als 80% vom Gesamtverbrauch")
                 if product == "lightcloud" and statement_config[product]["actual_usage_net"] <= 0:
                     statement["warnings"].append(f"{statement_config[product]['label']} Netzbezug ist nicht vorhanden")
