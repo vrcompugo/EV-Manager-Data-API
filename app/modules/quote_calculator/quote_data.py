ff0b02ab06e62a4dc0ecfbb3a7e142f8d5e011f8
Andreas Hedderich <a.hedderich@hbundb.de>
2022-05-18 09:34:36 +0200

    dach 12 wochen vorlauf

--- app/modules/quote_calculator/quote_data.py
+++ app/modules/quote_calculator/quote_data.py
@@ -120,7 +120,7 @@ def calculate_quote(lead_id, data=None, create_quote=False):
         return_data["heating_quote"]["has_special_condition"] = False
         return_data["bluegen_quote"]["has_special_condition"] = False
         if data.get("has_roof_reconstruction_quote") is True:
-            delivery_date = datetime.datetime.now() + datetime.timedelta(weeks=34)
+            delivery_date = datetime.datetime.now() + datetime.timedelta(weeks=12)
         elif data.get("has_heating_quote") is True:
             delivery_date = datetime.datetime.now() + datetime.timedelta(weeks=16)
         else:
