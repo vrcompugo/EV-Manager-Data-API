ad57fc6b437411938eed1961e674b9a6733b47bd
Andreas Hedderich <a.hedderich@hbundb.de>
2022-05-27 18:43:01 +0200

    re

--- app/modules/quote_calculator/cron.py
+++ app/modules/quote_calculator/cron.py
@@ -18,9 +18,10 @@ def cron_heatpump_auto_quote_generator():
     for deal in deals:
         if deal.get("contact_id") in [None, "", 0, "0"]:
             lead = get_lead(deal.get("unique_identifier"))
-            update_deal(deal["id"], {
-                "contact_id": lead.get("contact_id")
-            })
+            if lead is not None:
+                update_deal(deal["id"], {
+                    "contact_id": lead.get("contact_id")
+                })
 
     deals = get_deals({
         "SELECT": "full",
