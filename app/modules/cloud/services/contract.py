42697b192f7b70254ec534e32eb6596a39cfaa05
Andreas Hedderich <a.hedderich@hbundb.de>
2022-08-10 15:30:10 +0200

    re

--- app/modules/cloud/services/contract.py
+++ app/modules/cloud/services/contract.py
@@ -1424,10 +1424,10 @@ def find_credit_memo_bugs():
                     if payment.get("type") != "credit_note":
                         continue
                     contract_amount = contract_amount + payment.get("amountNet")
-            if contract_amount > 0:
+            if contract_amount > 40:
                 count = count + 1
                 print(deal.get("contract_number"))
                 print(contract_amount)
-            amount = amount + contract_amount
+                amount = amount + contract_amount
     print(count)
     print(amount)
