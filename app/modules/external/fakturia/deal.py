482aa33140136301cb0e364a1677d1014fd76427
Andreas Hedderich <a.hedderich@hbundb.de>
2022-08-17 15:45:07 +0200

    re

--- app/modules/external/fakturia/deal.py
+++ app/modules/external/fakturia/deal.py
@@ -409,7 +409,7 @@ def enhance_payment(item, item_type):
             item["service_year_net"][pay_item["performanceDateStart"][:4]] = pay_item["amountNetSum"]
             item["service_year"][pay_item["performanceDateStart"][:4]] = pay_item["amountNetSum"] * (1 + (pay_item["taxRatePercent"] / 100))
     item["amountGross_normalized"] = item["amountGross"]
-    if item_type in ["invoice_corrention", "credit_note"]:
+    if item_type in ["credit_note"]:
         item["amountGross_normalized"] = item["amountGross"] * -1
     return item
 
