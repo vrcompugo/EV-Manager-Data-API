6562e565ab3dc1f57440c288f4a47dcdd65b49d6
Andreas Hedderich <a.hedderich@hbundb.de>
2022-09-20 11:32:31 +0200

    re

--- app/modules/order/sherpa/excel/part01_general.py
+++ app/modules/order/sherpa/excel/part01_general.py
@@ -1,4 +1,6 @@
 import json
+from dateutil.parser import parse
+
 from app.models import Order
 
 from .utils import get_data_value
@@ -6,5 +8,5 @@ from .utils import get_data_value
 
 def part01_general(wb, excel_layout, deal, contact):
     if deal.get("wishdate") not in [None, ""]:
-        wb["Neukunden"]["D" + str(wb.current_row)] = deal.get("wishdate")
+        wb["Neukunden"]["D" + str(wb.current_row)] = parse(deal.get("wishdate")).strftime("%d.%m.%Y")
     return wb
