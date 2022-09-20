15a2d3cae6d458e96e26ad114c21c3148eaf6c36
Andreas Hedderich <a.hedderich@hbundb.de>
2022-09-20 11:14:44 +0200

    re

--- app/modules/order/sherpa/excel2/part01_general.py
+++ app/modules/order/sherpa/excel2/part01_general.py
@@ -6,4 +6,6 @@ from .utils import get_data_value
 
 def part01_general(wb, excel_layout, deal, contact):
     wb["Neukunden"]["A" + str(wb.current_row)] = deal.get("transaction_code")
+    if deal.get("wishdate") not in [None, ""]:
+        wb["Neukunden"]["D" + str(wb.current_row)] = deal.get("wishdate")
     return wb
