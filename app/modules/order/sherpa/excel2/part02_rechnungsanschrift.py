0bdaac72dc7216dfaf350fc82a014936db3ccd3a
Andreas Hedderich <a.hedderich@hbundb.de>
2022-10-28 09:34:22 +0200

    re

--- app/modules/order/sherpa/excel2/part02_rechnungsanschrift.py
+++ app/modules/order/sherpa/excel2/part02_rechnungsanschrift.py
@@ -5,15 +5,17 @@ from .utils import get_data_value
 
 
 def part02_rechnungsanschrift(wb, excel_layout, deal, contact):
-    contract_number = deal.get("contract_number")
-    if deal.get("is_cloud_consumer") in [True, "true", 1, "1"]:
-        contract_number = contract_number + "c1"
-        if deal.get("title").find(deal.get("contract_number") + "c2") >= 0:
-            contract_number = deal.get("contract_number") + "c2"
-    if deal.get("is_cloud_heatcloud") in [True, "true", 1, "1"]:
-        contract_number = contract_number + "w1"
-    if deal.get("is_cloud_ecloud") in [True, "true", 1, "1"]:
-        contract_number = contract_number + "ec"
+    from app.modules.cloud.services.contract import normalize_contract_number
+    contract_number = normalize_contract_number(deal.get("contract_number"))
+    if contract_number == deal.get("contract_number"):
+        if deal.get("is_cloud_consumer") in [True, "true", 1, "1"]:
+            contract_number = contract_number + "c1"
+            if deal.get("title").find(deal.get("contract_number") + "c2") >= 0:
+                contract_number = deal.get("contract_number") + "c2"
+        if deal.get("is_cloud_heatcloud") in [True, "true", 1, "1"]:
+            contract_number = contract_number + "w1"
+        if deal.get("is_cloud_ecloud") in [True, "true", 1, "1"]:
+            contract_number = contract_number + "ec"
     wb["Neukunden"]["S" + str(wb.current_row)] = contract_number
     wb["Neukunden"]["U" + str(wb.current_row)] = contact.get("company")
     wb["Neukunden"]["X" + str(wb.current_row)] = "Frau" if contact.get("salutation") == "ms" else "Herr"
