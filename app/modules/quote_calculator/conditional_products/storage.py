06d1466ee4d6ccf31ad9bfab2c17299452a97341
Andreas Hedderich <a.hedderich@hbundb.de>
2023-03-17 08:46:27 +0100

    re

--- app/modules/quote_calculator/conditional_products/storage.py
+++ app/modules/quote_calculator/conditional_products/storage.py
@@ -14,10 +14,10 @@ def add_product(data):
     if data["data"].get("heater_usage") not in [None, "", 0, "0"]:
         size = size + math.ceil(float(data["data"].get("heater_usage")) / 6100) * 2.5
     is_overwrite = False
+    if int(data["data"]["overwrite_storage_size"]) > size:
+        size = int(data["data"]["overwrite_storage_size"])
+        is_overwrite = True
     if "overwrite_storage_size" in data["data"] and str(data["data"]["overwrite_storage_size"]) in ["5", "7.5", "10"]:
-        if int(data["data"]["overwrite_storage_size"]) > size:
-            size = int(data["data"]["overwrite_storage_size"])
-            is_overwrite = True
         version = "Senec Lithium Speicher"
         stack_count = math.ceil((size - 2.5) / 2.5)
         if stack_count < 1:
